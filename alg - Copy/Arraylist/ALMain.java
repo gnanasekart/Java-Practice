246bf14c63f8f739de265af5a09fcc3205c9109e
gnanasekart <gnanasekar69@hotmail.com>
2020-06-29 12:22:59 +0530

    alg

---
+++ alg - Copy/Arraylist/ALMain.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class ALMain()
+{
+public Static void main(String[] args)
+{
+	List<Employee> emp = new ArrayList<>();
+	
+	emp.add(new Employee("gnana", "sekar", 123));
+	emp.add(new Employee("gowri", "shankar", 345));
+	emp.add(new Employee("mathan", "kumar", 456));
+	
+	for (int i=0; i<emp.length; i++)
+	{
+		System.out.println(Employee);
+	}
+}
+}
