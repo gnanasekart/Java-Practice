266fa4a89096df59e18297750d970b34c1e2bb55
t.gnanasekar <t.gnanasekar@temenos.com>
2022-08-03 19:00:19 +0530

    modified package

--- string/Searchchar.java
+++ string/Searchchar.java
@@ -4,34 +4,34 @@ import java.util.*;
 
 class Searchchar
 {
-public static void main(String[] args)
-{
-Scanner sc = new Scanner(System.in);
-System.out.println("enter char");
-String[] a = sc.next();
-char[] b = a.toCharArray();
-System.out.println(a);
-System.out.println("enter char to search");
-String c = sc.next();
-char d = c.charAt(0);
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter char");
+		String a = sc.next();
+		char[] b = a.toCharArray();
+		System.out.println(a);
+		System.out.println("enter char to search");
+		String c = sc.next();
+		char d = c.charAt(0);
 
-for (int i=0; i<=a.length; i++)
-{
-if(b[i] == d)
-{
-if(d>= 'a' && d <= 'z')
-{
-d-=32;
-}
-else if(d >= 'A' && d <= 'Z')
-{
-d+=32;
-}
-a[i]=d;
-break;
-}
-}
-//sc=String.valueOf(a);
-//System.out.println(sc);
-}
+		for (int i=0; i<=a.length(); i++)
+		{
+			if(b[i] == d)
+			{
+				if(d>= 'a' && d <= 'z')
+				{
+					d-=32;
+				}
+				else if(d >= 'A' && d <= 'Z')
+				{
+					d+=32;
+				}
+				//a[i]=d;
+				break;
+			}
+		}
+		//sc=String.valueOf(a);
+		//System.out.println(sc);
+	}
 }
