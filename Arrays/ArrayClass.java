3cb273e7d30d4d870aa4fd960ba4b2e3cde18222
t.gnanasekar <t.gnanasekar@temenos.com>
2022-08-11 20:51:02 +0530

    update

---
+++ Arrays/ArrayClass.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ArrayClass {
+
+	public static void main(String[] args) {
+	
+	int[] one = {5, 2, 7, 1, 4, 7, 3};
+	int[] two = Arrays.copyOf(one, one.length);
+	
+	 Arrays.sort(one);
+	 
+	 //Integer.compare(one, two);
+	 
+	for (int i = 0; i < one.length; i++) {
+		Integer.compare(two[i], one[i]);
+		System.out.println(one[i]);
+	}
+	
+	}
+	
+}
