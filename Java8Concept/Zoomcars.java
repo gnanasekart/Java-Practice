d5f3aeb9410b6d700b06193c0783eefcab399f2f
t.gnanasekar <t.gnanasekar@temenos.com>
2022-09-06 21:29:49 +0530

    practice

---
+++ Java8Concept/Zoomcars.java
@@ -0,0 +1,44 @@
+package Java8Concept;
+
+public class Zoomcars {
+	
+	private String model;
+	private String name;
+	private long price;
+	
+	public Zoomcars(String model, String name, int price) {
+		super();
+		this.model = model;
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "model=" + model + ", name=" + name + ", price=" + price;
+	}
+}
