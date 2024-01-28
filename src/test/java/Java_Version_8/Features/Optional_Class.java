99470a48ca21432269effe982cfd1713ef89a979
Gnana sekar T <64648019+gnanasekart@users.noreply.github.com>
2024-01-28 19:23:49 +0530

    Optional

---
+++ src/test/java/Java_Version_8/Features/Optional_Class.java
@@ -0,0 +1,21 @@
+package Java_Version_8.Features;
+
+import Java_Version_8.java8Stream.Article;
+
+import java.util.Optional;
+
+public class Optional_Class {
+
+    public static void main(String[] args) {
+       // Optional<Article> p = Optional.empty();
+
+        Article ar = new Article();
+        //Optional<Article> p = Optional.of(ar);
+
+
+
+        Optional<Article> p1 = Optional.ofNullable(ar);
+
+        System.out.println(p1);
+    }
+}
